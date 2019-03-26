aea766448ebda2f160bb3226b03cc6345e3c2663
Jean Pignouf <ratouney1998@gmail.com>
2019-03-26 15:24:05 +0100

    Merge i guess

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,17 +6,21 @@ public class PlayerController : MonoBehaviour {
 
     // Movement is handled by a separate script
     public PlayerMovement pm;
+    public Sprite standing;
+    public Sprite crouched;
 
     public float movementSpeed = 100;
     public float jumpHeight = 35;
     public int maxJumps = 1;
     public int resetOnWallLatch = 1;
 
-
     private Rigidbody2D rb;
-    private bool airborne = false;
+    private SpriteRenderer sr;
 
-    private bool facingLeft = false;
+    private bool airborne = false;
+    private bool isCrouched = false;
+
+    private bool facingLeft = true;
     private float horizontalMovement;
 
     private bool requestJump = false;
@@ -25,6 +29,9 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        Component.FindChi
     }  
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -56,6 +63,17 @@ public class PlayerController : MonoBehaviour {
         } else if (horizontalMovement < 0 && !facingLeft)
         {
             DoAFlip();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            isCrouched = true;
+            sr.sprite = crouched;
+        }
+        if (isCrouched && Input.GetKeyUp(KeyCode.S))
+        {
+            isCrouched = false;
+            sr.sprite = standing;
         }
     }
 
