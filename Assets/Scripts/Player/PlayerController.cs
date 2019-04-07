bc928b842c2191b6d5bb6bc023c0db33641b21b9
Jean Pignouf <ratouney1998@gmail.com>
2019-04-07 14:46:20 +0200

    Player movements are fluid, added constraint to not bug into objects with wall tag

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    private int x = 0;
-    public int speed = 5;
     public Transform groundChecker;
+    public float jumpVelocity = 10;
+    public int speed = 5;
+
+    public int maxJumps = 1;
+
+    private int currentJumps = 0;
+
+    private bool wallLatch = false;
+    private int latchDirection = 0;
+    private int movementDirection = 0;
+    private float defaultGravity;
     private bool isGrounded = true;
     private Rigidbody2D rb;
     private int layerMask;
-    public float jumpVelocity = 10;
     private Animator anim;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        defaultGravity = rb.gravityScale;
         layerMask = 1 << 10;
         layerMask = ~layerMask;
     }
@@ -25,19 +34,27 @@ public class PlayerController : MonoBehaviour {
     {
         Move();
         GroundRaycast();
-        Jump();   
+        Jump();
         anim.SetBool("jumping", !isGrounded);
     }
 
     private void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        if (Input.GetKeyDown(KeyCode.Space) && maxJumps > currentJumps)
         {
-            Debug.Log("I m in");
             rb.velocity = Vector2.up * jumpVelocity * Time.deltaTime;
+            currentJumps++;
+            /*
+            if (wallLatch)
+                rb.gravityScale = defaultGravity;
+            wallLatch = false;
+            */
+            // Debug.Log("Jumps : " + currentJumps);
         }
         if (rb.velocity.y < 0 && isGrounded == false)
+        {
             rb.velocity += Vector2.up * Physics2D.gravity.y * 1.5f * Time.deltaTime;
+        }
         else if (rb.velocity.y > 0 && isGrounded == false)
             rb.velocity += Vector2.up * Physics2D.gravity.y * 1f * Time.deltaTime;
     }
@@ -46,20 +63,73 @@ public class PlayerController : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.D))
         {
-            x = 1;
+            movementDirection = 1;
             anim.SetBool("running", true);
         }
         else if (Input.GetKey(KeyCode.A))
         {
-            x = -1;
+            movementDirection = -1;
             anim.SetBool("running", true);
         }
         else
         {
-            x = 0;
+            movementDirection = 0;
             anim.SetBool("running", false);
         }
-        transform.Translate(new Vector2(x, 0) * Time.deltaTime * speed);
+        
+        if (!wallLatch || movementDirection == latchDirection)
+            transform.Translate(new Vector2(movementDirection, 0) * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        // https://answers.unity.com/questions/783377/detect-side-of-collision-in-box-collider-2d.html
+
+        Collider2D collider = collision.collider;
+        bool collideFromLeft = false;
+        bool collideFromTop = false;
+        bool collideFromRight = false;
+        bool collideFromBottom = false;
+        float RectWidth = this.GetComponent<Collider2D>().bounds.size.x;
+        float RectHeight = this.GetComponent<Collider2D>().bounds.size.y;
+        float circleRad = collider.bounds.size.x;
+
+        if (collision.gameObject.tag.Contains("Terrain-Wall"))
+        {
+            Vector3 contactPoint = collision.contacts[0].point;
+            Vector3 center = collider.bounds.center;
+
+            if (contactPoint.y > center.y && //checks that circle is on top of rectangle
+                (contactPoint.x < center.x + RectWidth / 2 && contactPoint.x > center.x - RectWidth / 2))
+            {
+                collideFromTop = true;
+            }
+            else if (contactPoint.y < center.y &&
+                (contactPoint.x < center.x + RectWidth / 2 && contactPoint.x > center.x - RectWidth / 2))
+            {
+                collideFromBottom = true;
+            }
+            else if (contactPoint.x > center.x &&
+                (contactPoint.y < center.y + RectHeight / 2 && contactPoint.y > center.y - RectHeight / 2))
+            {
+                collideFromRight = true;
+            }
+            else if (contactPoint.x < center.x &&
+                (contactPoint.y < center.y + RectHeight / 2 && contactPoint.y > center.y - RectHeight / 2))
+            {
+                collideFromLeft = true;
+            }
+
+            if (currentJumps > 0)
+                currentJumps -= 1;
+            wallLatch = true;
+            if (collideFromLeft)
+                latchDirection = -1;
+            if (collideFromRight)
+                latchDirection = 1;
+        }
+
     }
 
     private void GroundRaycast()
@@ -68,11 +138,21 @@ public class PlayerController : MonoBehaviour {
 
 
         hit = Physics2D.Raycast(groundChecker.position, -Vector2.up, 0.5f, layerMask);
-        Debug.DrawRay(groundChecker.position, new Vector2(0, -0.5f));
+        // Debug.DrawRay(groundChecker.position, new Vector2(0, -0.5f));
 
         if (hit.collider != null)
         {
             isGrounded = true;
+            /*
+            if (wallLatch)
+                rb.gravityScale = defaultGravity;
+            */
+            wallLatch = false;
+            if (currentJumps != 0)
+            {
+                currentJumps = 0;
+                // Debug.Log("Jumps reset !");
+            }
         }
         else
         {
