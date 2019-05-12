1cdeb2e0c85d6f2decfe3b739b44c590c02d137d
huntmaster13 <47777551+huntmaster13@users.noreply.github.com>
2019-05-12 13:50:38 +0200

    Improved pause menu

--- Assets/PauseMenu/PauseMenu.cs
+++ Assets/PauseMenu/PauseMenu.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class PauseMenu : MonoBehaviour
 {
     public static bool GameIsPaused = false;
     public GameObject pauseMenuUI;
+    public GameObject optionMenuUI;
+    public GameObject goResumeButton;
+    public GameObject goBackButton;
+    public GameObject goOptionButton;
+    private Button bBackButton;
+    private Button bResumeButton;
+    private Button bOptionButton;
 
     // Update is called once per frame
     void Update()
     {
+        /*Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        est-ce nécessaire?
+         */
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (GameIsPaused)
@@ -30,11 +44,35 @@ public class PauseMenu : MonoBehaviour
         GameIsPaused = false;
     }
 
+    public void Option()
+    {
+        pauseMenuUI.SetActive(false);
+        optionMenuUI.SetActive(true);
+
+        bBackButton = goBackButton.GetComponent<Button>();
+        bBackButton.Select();
+        bBackButton.OnSelect(null);
+    }
+
+    public void BackButton()
+    {
+        optionMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+
+        bOptionButton = goOptionButton.GetComponent<Button>();
+        bOptionButton.Select();
+        bOptionButton.OnSelect(null);
+    }
+
     void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+
+        bResumeButton = goResumeButton.GetComponent<Button>();
+        bResumeButton.Select();
+        bResumeButton.OnSelect(null);
     }
 
     public void LoadMenu()
